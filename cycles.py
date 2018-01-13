d9a50ccebd26a10464cd72a3a7b548ff8b26cd0e
Alex <acwang@berkeley.edu>
2018-01-13 13:17:23 -0500

    preliminary cycles and added an in file

--- cycles.py
+++ cycles.py
@@ -3,16 +3,34 @@
 import os
 import sys
 
-def main():
-	# first make a cycle detector
-	order = ["a", "b", "c", "a"]
-	seen = set()
-	while !(order == null):
-		a = order.pop()
+def check(dict, ID):
+	temp = dict[ID]
+	while (not temp is null):
+		a = temp.pop()
 		if a in seen:
-			print("Cycle")
+			print("Cycle with plate: {0} | Cycle: {1}".format(ID, dict[ID]))
 			return
 		else:
 			seen.add(a)
 
+def main():
+	# first make a cycle detector
+	order = {}
+	
+	seen = set()
+
+	f = os.open("routes.in")
+	line = f.nextLine()
+	while (line is not null):
+		vals = line.split(" ")
+		node = vals[0]
+		plate = vals[1]
+		if plate not in order.keys():
+			order[plate] = [node]
+		else:
+			order[plate].append(node)
+
+		
+	for key in order.keys():
+		check(order, key)
 main()
